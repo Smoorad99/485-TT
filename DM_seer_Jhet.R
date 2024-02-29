83b71ca0cb0e3d69f97c27c4f7551548306ba777
Jhet Cabigas <cjhet02@gmail.com>
2024-02-29 14:37:30 -0800

    Variable renames

--- DM_seer_Jhet.R
+++ DM_seer_Jhet.R
@@ -10,12 +10,12 @@ subset[subset == "Blank(s)"] <- NA
 # change vars to factor or numeric where appropriate
 subset <- subset %>% mutate(
   Sex = as.factor(Sex),
-  `Race and origin recode (NHW, NHB, NHAIAN, NHAPI, Hispanic)` = as.factor(`Race and origin recode (NHW, NHB, NHAIAN, NHAPI, Hispanic)`),
-  `Age recode with <1 year olds` = as.factor(`Age recode with <1 year olds`),
-  `RX Summ--Scope Reg LN Sur (2003+)` = as.factor(`RX Summ--Scope Reg LN Sur (2003+)`),
-  `RX Summ--Surg Oth Reg/Dis (2003+)` = as.factor(`RX Summ--Surg Oth Reg/Dis (2003+)`),
-  `RX Summ--Surg/Rad Seq` = as.factor(`RX Summ--Surg/Rad Seq`),
-  `RX Summ--Reg LN Examined (1998-2002)` = as.numeric(`RX Summ--Reg LN Examined (1998-2002)`),
-  `RX Summ--Systemic/Sur Seq (2007+)` = as.factor(`RX Summ--Systemic/Sur Seq (2007+)`),
-  `Months from diagnosis to treatment` = as.numeric(`Months from diagnosis to treatment`)
+  Race.origin = as.factor(`Race and origin recode (NHW, NHB, NHAIAN, NHAPI, Hispanic)`),
+  Age = as.factor(`Age recode with <1 year olds`),
+  Scope.Reg.LN.Sur = as.factor(`RX Summ--Scope Reg LN Sur (2003+)`),
+  Surg.Oth.Reg.Dis = as.factor(`RX Summ--Surg Oth Reg/Dis (2003+)`),
+  Surg.Rad.Seq = as.factor(`RX Summ--Surg/Rad Seq`),
+  Reg.LN.exam = as.numeric(`RX Summ--Reg LN Examined (1998-2002)`),
+  Systemic.Sur.Seq = as.factor(`RX Summ--Systemic/Sur Seq (2007+)`),
+  Months.diag.to.treat = as.numeric(`Months from diagnosis to treatment`)
 )
