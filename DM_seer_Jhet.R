b8a66f2d75bac7536088e001ce86b4478846881a
Jhet Cabigas <cjhet02@gmail.com>
2024-02-29 16:30:11 -0800

    dropped old columns, race.origin

--- DM_seer_Jhet.R
+++ DM_seer_Jhet.R
@@ -12,13 +12,11 @@ subset[subset == "Blank(s)"] <- NA
 
 # change vars to factor or numeric where appropriate
 subset <- subset %>% 
-  select(-c(Age.recode.with..1.year.olds, RX.Summ..Reg.LN.Examined..1998.2002.)) %>% 
     mutate(
     Sex = as.factor(Sex),
-    Race.origin = as.factor(Race.and.origin.recode..NHW..NHB..NHAIAN..NHAPI..Hispanic.),
     Scope.Reg.LN.Sur = as.factor(RX.Summ..Scope.Reg.LN.Sur..2003..),
     Surg.Oth.Reg.Dis = as.factor(RX.Summ..Surg.Oth.Reg.Dis..2003..),
     Surg.Rad.Seq = as.factor(RX.Summ..Surg.Rad.Seq),
     Systemic.Sur.Seq = as.factor(RX.Summ..Systemic.Sur.Seq..2007..),
     Months.diag.to.treat = as.numeric(Months.from.diagnosis.to.treatment)
-)
+) %>% select(-c(2:11))
